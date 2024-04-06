c5071bd549f74c88d12532ffb0853323fbedf379
dennis2908 <manullang_d@yahoo.com>
2024-04-06 20:23:40 +0700

    task

--- src/app/(app-layout)/checkout/page.tsx
+++ src/app/(app-layout)/checkout/page.tsx
@@ -8,7 +8,7 @@ import { clearCart } from '@/cart/cart-actions';
 
 import { useEffect } from 'react';
 
-export default function CheckoutPage(context: { params: object }) {
+export default function CheckoutPage(context: { params: {} }) {
   // useEffect(() => {
   //   const storeCall = async () => {
   //     setformData({
@@ -34,14 +34,22 @@ export default function CheckoutPage(context: { params: object }) {
   useEffect(() => {
     console.log(111, context);
     const getData = async () => {
-      // await clearCart();
-
       await axios.post('http://127.0.0.1:8005/book', {
         email: storeLogin.getState().payload.authEmail,
         fullname: storeLogin.getState().payload.authFullName,
-        total: JSON.parse(context.params.searchParams.order).totalPrice,
-        order: JSON.parse(context.params.searchParams.order),
+        total: JSON.parse(context.searchParams.order).totalPrice,
+        order: JSON.parse(context.searchParams.order),
       });
+
+      storeLogin.dispatch({
+        type: 'CHANGE_STATE',
+        payload: {
+          authFullName: '',
+          authEmail: '',
+        },
+      });
+
+      await clearCart();
 
       location.href = '/search';
     };
